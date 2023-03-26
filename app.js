6624e73449721ca04c3e69b61d696218d75cb784
Juan Suñe <111535673+JuanSune@users.noreply.github.com>
2023-03-26 17:26:04 -0300

    n

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ mongoose.connect('mongodb://127.0.0.1/test').then(() => {
     console.log("Houve o seguinte erro"+err)
 })
 
-const UserSchema = mongoose.Schema({
-    
+const UsuarioSchema = mongoose.Schema({
+    nome:{
+        type:String,
+        require:true
+    },
+    sobrenome:{
+        type:String,
+        require:true
+    }
+})
+
+mongoose.model('usuarios', UsuarioSchema)
+
+const Victor = mongoose.model('usuarios')
+
+new Victor({
+    nome:'Juanzito',
+    sobrenome:'Yami'
+}).save().then(() => {
+    console.log("Foi cadastrado o usuario")
+}).catch((err) => {
+    console.log("Deu o seguinte erro: "+err)
 })
