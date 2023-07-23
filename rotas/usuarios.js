ab5dd7e44a8e0fb1ec4b2e6d749272d75afe93f5
Juan Suñe <111535673+JuanSune@users.noreply.github.com>
2023-07-23 17:59:15 -0300

    t

--- rotas/usuarios.js
+++ rotas/usuarios.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const rota = express.Router();
-const moongoseUser = require('mongoose');
+const mongooseModule = require("mongoose");
 require('../models/Usuario')
 
-const user = moongoseUser.model('User');
+const userDaqui = mongooseModule.model("User");
+
+const bcrypt = require('bcryptjs')
 
 rota.get("/registro", (req, res) => {
     res.render("usuario/registro")
@@ -42,6 +44,34 @@ rota.post("/registro", (req, res) => {
         res.render("usuario/registro", {erros: erros})
     }else{
         //proxima aula
+        userDaqui.findOne({email: req.body.email}).then((user) => {
+            if(user){
+                //msg_sucesso
+                req.flash('msg_erro',"Já existe essa conta com esse email no sistema")
+                res.redirect("/usuarios/registro")
+            }
+            else{
+
+                const NovoUser = new userDaqui ({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
+                
+                bcrypt.genSalt(10,(erro, salt) =>{
+                    bcrypt.hash(NovoUser.senha, salt, (erro, valor) =>{
+                        NovoUser.senha = valor
+                        NovoUser.save().then(() =>{
+                            req.flash('msg_sucesso',"Deu certo porra")
+                            res.redirect("/")
+                        }).catch((err) =>{
+                            req.flash('msg_erro',"Deu ERRADO porra")
+                            res.redirect("/")
+                        })
+                    })
+                })
+            }
+        })
     }
     
        
