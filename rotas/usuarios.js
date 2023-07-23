fecbac832185a93ba883be91c7e267267ff1a290
Juan Suñe <111535673+JuanSune@users.noreply.github.com>
2023-07-22 21:55:02 -0300

    h

--- rotas/usuarios.js
+++ rotas/usuarios.js
@@ -9,5 +9,44 @@ rota.get("/registro", (req, res) => {
     res.render("usuario/registro")
 })
 
+rota.post("/registro", (req, res) => {
+
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || 
+        req.body.nome == null){
+            erros.push({texto: "Nome invalido"})
+    }
+
+    if(!req.body.email || typeof req.body.email == undefined || 
+        req.body.email == null){
+            erros.push({texto: "E-mail invalido"})
+
+    }
+
+    if(!req.body.senha || typeof req.body.senha == undefined || 
+        req.body.senha == null){
+            erros.push({texto: "Senha invalida"})
+
+    }
+
+    if(req.body.senha.length < 4){ 
+        erros.push({texto: "Senha muito curta"})
+    }
+
+    if(req.body.senha != req.body.senha2){ 
+        erros.push({texto: "As senhas sao diferentes"})
+    }
+
+    if(erros.length > 0){
+        res.render("usuario/registro", {erros: erros})
+    }else{
+        //proxima aula
+    }
+    
+       
+
+    
+})
 
 module.exports = rota;
